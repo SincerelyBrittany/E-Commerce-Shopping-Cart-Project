1d03062c7e0b5938a7be2c29a456d7a69e39e3fd
Sincerely Brittany <sincerelybr@gmail.com>
2023-02-06 22:45:46 -0500

    adds auth funcationality

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -1,11 +1,11 @@
-import {  createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { isLoggedIn },
+  initialState: { isLoggedIn: false },
   reducers: {
     login(state) {
-      state.isLoggedIn = true; //reduxjs/toolkit makes a copy of state for you, so you are not overriding state here
+      state.isLoggedIn = true;
     },
     logout(state) {
       state.isLoggedIn = false;
@@ -13,6 +13,6 @@ const authSlice = createSlice({
   },
 });
 
-const authActions = authSlice.actions;
+export const authActions = authSlice.actions;
 
 export default authSlice;
